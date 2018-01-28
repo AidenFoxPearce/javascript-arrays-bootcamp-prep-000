3af104ec76d970c0c4536bad2fb77a5e217a8240
BRAULIO R URBINA <braulio_urbina@hotmail.com>
2018-01-28 18:07:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,9 +5,13 @@ function addElementToBeginningOfArray(a, e) {
 }
 
 function destructivelyAddElementToBeginningOfArray(a, e) {
+<<<<<<< HEAD
   a.unshift(e);
   
   return a;
+=======
+  return a.unshift(e);
+>>>>>>> ce0ad8395d7973f3f8560c16667d6f71ed8bf94f
 }
 
 function addElementToEndOfArray(a, e) {
@@ -15,9 +19,13 @@ function addElementToEndOfArray(a, e) {
 }
 
 function destructivelyAddElementToEndOfArray(a, e) {
+<<<<<<< HEAD
   a.push(e);
   
   return a;
+=======
+  return a.push(e);
+>>>>>>> ce0ad8395d7973f3f8560c16667d6f71ed8bf94f
 }
 
 function accessElementInArray(a, i) {
@@ -25,9 +33,13 @@ function accessElementInArray(a, i) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(a) {
+<<<<<<< HEAD
   a.shift();
   
   return a;
+=======
+  return a.shift();
+>>>>>>> ce0ad8395d7973f3f8560c16667d6f71ed8bf94f
 }
 
 function removeElementFromBeginningOfArray(a) {
@@ -35,9 +47,13 @@ function removeElementFromBeginningOfArray(a) {
 }
 
 function destructivelyRemoveElementFromEndOfArray(a) {
+<<<<<<< HEAD
   a.pop();
   
   return a;
+=======
+  return a.pop();
+>>>>>>> ce0ad8395d7973f3f8560c16667d6f71ed8bf94f
 }
 
 function removeElementFromEndOfArray(a) {
